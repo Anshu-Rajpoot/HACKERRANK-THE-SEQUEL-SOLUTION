f643ec03f02cee8e9506444614cd794d144f9c9f
Anshu Rajpoot <107042248+Anshu-Rajpoot@users.noreply.github.com>
2023-03-14 18:43:19 +0530

    Add files via upload

---
+++ HACKERRANK PROBLEM THE SEQUEL.sql
@@ -0,0 +1,10 @@
+--SIMPLY SQL-THE SEQUEL--
+--Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard! Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge. Order your output in descending order by the total number of challenges in which the hacker earned a full score. If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id.--
+SELECT H.hacker_id, H.name 
+FROM   submissions S 
+   JOIN challenges C ON S.challenge_id = C.challenge_id 
+   JOIN difficulty D ON C.difficulty_level = D.difficulty_level 
+   JOIN hackers H ON S.hacker_id = H.hacker_id AND S.score = D.score 
+GROUP  BY H.hacker_id, H.name 
+HAVING Count(S.hacker_id) > 1 
+ORDER  BY Count(S.hacker_id) DESC,  S.hacker_id ASC;
